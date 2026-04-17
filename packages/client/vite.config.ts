2a4be091fc4d49f1e59cb7a37174cc2fef9a22c5
SiGMan <sigman@ioupg.com>
2026-04-17 02:08:31 -0700

    chore: add svelte + vite plugin

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  plugins: [svelte()],
   server: {
     host: '0.0.0.0',
     port: 5173
